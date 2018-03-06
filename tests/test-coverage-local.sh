a3bf436664b1e1ef538f045c6d4752b8ad2a7359
Joe Zhu <sha.joe.zhu@gmail.com>
2018-03-06 01:16:20 +0000

    update

---
+++ tests/test-coverage-local.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Generate gcov output
+#make -mj
+
+# Generate html report
+lcov --base-directory . --directory . --zerocounters -q
+make check -mj
+lcov --base-directory . --directory . -c -o libbash_test.info
+# --rc lcov_branch_coverage=1 option will turn on branch check
+lcov --remove libbash_test.info "/usr*" "tests/unittest/*" -o libbash_test.info # remove output for external libraries
+rm -rf ./testCoverage
+genhtml -o ./testCoverage -t "libbash test coverage" --num-spaces 4 libbash_test.info --legend --function-coverage --demangle-cpp
