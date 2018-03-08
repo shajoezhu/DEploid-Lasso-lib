5005624f0ca6957ba725c28ac392f018c2d6223c
Joe Zhu <sha.joe.zhu@gmail.com>
2018-03-08 21:47:48 +0000

    update read panel and wsaf

--- main.cpp
+++ main.cpp
@@ -5,10 +5,10 @@ using namespace std;
 
 int main(int argc, char *argv[]){
     try {
-        vector < vector <double> > matrix = TxtReader ("data/panel_chrom1.txt").matrix;
-        vector < double > wsaf = TxtReader ("data/PG0402-C_chrom1.wsaf").vec;
-        //vector < double > wsaf = TxtReader ("data/PG0402-C_chrom2.wsaf").vec;
-        //vector < vector <double> > matrix = TxtReader ("data/panel_chrom2.txt").matrix;
+        vector < vector <double> > matrix = TxtReader (argv[1]).matrix;
+        vector < double > wsaf = TxtReader (argv[2]).vec;
+        //vector < vector <double> > matrix = TxtReader ("data/panel_chrom1.txt").matrix;
+        //vector < double > wsaf = TxtReader ("data/PG0402-C_chrom1.wsaf").vec;
         //vector < vector <double> > matrix = TxtReader ("data/myX.txt").matrix;
         //vector < double > wsaf = TxtReader ("data/myy.txt").vec;
         DEploidLASSO dummy(matrix, wsaf, 100);
--- utility/script.r
+++ utility/script.r
@@ -15,16 +15,21 @@ mybeta = read.table(paste(inFileName, ".BETA", sep=""), header=T, stringsAsFacto
 
 pdf("tmp.pdf")
 
-plot(mytable$df)
+plot(mytable$df, ylab = "# of predictor variable")
 
-plot(mytable$rsq)
+plot(mytable$rsq, ylab = "Deviance ratio")
 
-plot(diff(mytable$rsq))
+plot(diff(mytable$rsq), ylab = "Deviance ratio differences" )
 
+v_idx = c()
 plot(c(0, max(mytable$L1norm)),
-     c(min(mybeta[,-1]), max(mybeta[,-1])), type="n")
+     c(min(mybeta[,-1]), max(mybeta[,-1])), type="n", xlab = "L1 Norm", ylab = "Coefficient")
 for (i in 1:dim(mybeta)[1]){
-    lines(c(0,mytable$L1norm), c(0,mybeta[i,-1]))
+#    if (sum(mybeta[i,-1])>0){
+     lines(c(0,mytable$L1norm), c(0,mybeta[i,-1]), col = i)
+#        v_idx = c(v_idx, i)
+#    }
 }
+legend("topleft", legend=mybeta[,1], col = 1:length(mybeta[,1]), lty=1)
 
 dev.off()
